fc2b57fdc8e2fd0bfb11f7656b956311c4b00d9b
Garrett <me@garrettdowd.com>
2020-03-05 16:55:47 -0500

    Add deleted expense handling

--- splitwise_export.py
+++ splitwise_export.py
@@ -3,6 +3,23 @@ import pandas as pd
 import json
 import os
 
+def yes_or_no(question, default = None):
+    while "the answer is invalid":
+        if default == None:
+            reply = str(input(question+' (y/n): ')).lower().strip()
+        elif default == True:
+            reply = str(input(question+' (Y/n): ')).lower().strip()
+            if reply == '':
+                return True
+        elif default == False:
+            reply = str(input(question+' (y/N): ')).lower().strip()
+            if reply == '':
+                return False
+        if reply[:1] == 'y':
+            return True
+        if reply[:1] == 'n':
+            return False
+
 def authorize(path_to_auth = None):
     if path_to_auth == None:
         path_to_auth = 'auth.json'
@@ -39,9 +56,9 @@ def authorize(path_to_auth = None):
     return sObj
 
 
-def export_group(sObj, group_id = None):
+def get_group_expenses(sObj, group_id = None):
     if sObj == None:
-        print("No Splitwise object")
+        print("No Splitwise object given")
         return 0
     if group_id == None:
         groups = sObj.getGroups()
@@ -60,36 +77,57 @@ def export_group(sObj, group_id = None):
 
     expenses = sObj.getExpenses(offset,limit,group_id,friendship_id,dated_after,dated_before,updated_after,updated_before)
 
-    filepath = input("Enter filename. Leave blank for default. File will be saved in current directory\n")
-    if not filepath:
-        filepath = None
-    to_csv(expenses,filepath)
+    return expenses
+    
+def get_user_name(user):
+    if user != None:
+        return user.getFirstName() + " " + user.getLastName()
+    else:
+        return None
 
+def expenses_to_csv(expenses, filepath = None, include_deleted = None):
 
-def to_csv(expenses, filepath = None):
     if filepath == None:
-        filepath = 'data_export.csv'
+        filepath = input("Enter filename. Leave blank for default. File will be saved in current directory\n")
+        if not filepath:
+            filepath = 'data_export.csv'
 
+    if include_deleted == None:
+        include_deleted = yes_or_no("Include deleted expenses?", False)
+
+    # column_order = []
     df = []
     for expense in expenses:
         df_d = {
+            'Description': expense.getDescription(),
             'Date': expense.getDate(),
             'Category': expense.getCategory().getName(),
-            'Description': expense.getDescription(),
             'Details' : expense.getDetails(),
             'Cost': expense.getCost(),
             'Currency': expense.getCurrencyCode(),
             'Receipt': expense.getReceipt().getOriginal(),
+            'Deleted': expense.getDeletedBy(),
         }
         df.append(df_d)
 
     df = pd.DataFrame(df)
+
+    if include_deleted:
+        df['Deleted'] = df.apply(lambda row: get_user_name(row['Deleted']), axis=1)
+    else:
+        # Delete these row indexes from dataFrame
+        df = df[df['Deleted'].isna()]
+        # Delete Column
+        df = df.drop(columns=['Deleted'])
+    
+    # df = df.reindex(columns=column_uav)  # ensure columns are in correct order
     df.to_csv(filepath, encoding='utf-8', index=False)
 
 
 def main():
     sObj = authorize()
-    export_group(sObj)
+    expenses = get_group_expenses(sObj)
+    expenses_to_csv(expenses)
 
 if __name__ == '__main__':
     main()
